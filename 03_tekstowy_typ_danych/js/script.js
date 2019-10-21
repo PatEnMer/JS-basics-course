99d878fc5f4b4845274e9f3c0e4f831633ddd696
PatEnMer <patryk.nowak@gmail.com>
2019-10-21 17:23:04 +0200

    strings

---
+++ 03_tekstowy_typ_danych/js/script.js
@@ -0,0 +1,8 @@
+const firstName = 'Jan';
+// string - wartość tekstowa
+const lastName = 'Nowak';
+
+let bookTitle = '"Władca pierścieni"';
+
+let multiLine = 'Linijka pierwsza\nlinijka druga\n\ttrzecialinijka';
+// tworzenie nowych lini w kodzie, do wykorzystania później przez formatowanie css - white-space
