10f839941e628ab03aaaba94ba85951462738dd1
PatEnMer <patryk.nowak@gmail.com>
2019-10-22 18:08:26 +0200

    increment decrement

---
+++ 10_inkrementacja_dekrementacja/js/script.js
@@ -0,0 +1,24 @@
+let number1 = 0;
+let number2 = 5;
+
+number1 = number1 + 1;
+number1 += 1;
+number1++;
+
+// post inkrementacja - najpierw zwracana jest warość następnie zwiększana o 1
+// console.log(number1++);
+
+// preinkrementacja
+++number1;
+console.log(++number1);
+
+// dekrementacja
+number2 = number2 - 1;
+number2 -= 1;
+
+// console.log(number2--);
+
+// predekrementacja
+console.log(--number2);
+
+// inkrementacja i dekrementacja zmienia wartość zmiennej
