225caaad006caa8a23317f214603777905794285
PatEnMer <patryk.nowak@gmail.com>
2019-10-22 12:43:37 +0200

    null undefined

---
+++ 06_null_undefined/js/script.js
@@ -0,0 +1,12 @@
+// let yourName;
+// undefined
+
+let yourName = null;
+// null rozpoznawany jest jako object, jednak nim nie jest. nie poprawiony błąd dawnej składni JS
+yourName = 'Janek';
+
+let person = {
+	name: null
+};
+//jeżeli odnajduję wartość null, to została ona celowo przypisana w kodzie i zmienna jest wykorzystywana/do wykorzystania
+// warto undefined oznacza, że zmienna lub jej wartość nie zostały zdefiniowane
