e535699dbd16efc67b33d94a0179cc8e9afda04e
PatEnMer <patryk.nowak@gmail.com>
2019-10-23 17:48:27 +0200

    function as object method

---
+++ 17_funkcje_jako_metody/js/script.js
@@ -0,0 +1,14 @@
+let person = {
+	firstName: 'Jan',
+	lastName: 'Kowalski',
+	sayHello: function() {
+		console.log('cześć mam na imię ' + this.firstName);
+	},
+	setFirstName: function(firstName) {
+		this.firstName = firstName;
+	}
+};
+
+person.firstName = 'Piotr';
+
+person.sayHello();
