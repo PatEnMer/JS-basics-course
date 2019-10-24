c36eef44636d5af54ed38fe1d4235d3dcd3f52c4
PatEnMer <patryk.nowak@gmail.com>
2019-10-24 17:11:45 +0200

    adding to array

---
+++ 20_dodawanie_el_do_tablicy/js/script.js
@@ -0,0 +1,14 @@
+let arr = [];
+
+arr[0] = 'Kasia';
+arr[1] = 'Tomek';
+
+// arr[10] = 33;
+
+arr[arr.length] = 'Piotrek';
+
+arr.push('Monika'); //dodaje na koniec do tabeli
+arr.unshift('Wojtek');
+
+console.log(arr);
+console.log(arr.length);
