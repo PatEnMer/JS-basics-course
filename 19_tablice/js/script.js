ad41718b58aa4b301300980377371dc1cbd3626e
PatEnMer <patryk.nowak@gmail.com>
2019-10-24 13:54:39 +0200

    arrays create

---
+++ 19_tablice/js/script.js
@@ -0,0 +1,12 @@
+let numbers = [ 1, 3, 4, 5 ];
+
+console.log(numbers[2]);
+console.log(numbers.length);
+
+let things = [ { firstname: 'Anna' }, 2, 2 + 8 ];
+
+console.log(things[0].firstname); //do odpalenia w node.js
+
+let arrays = [ [ 2, 3, 4 ], [ 4, 2, 1, [ 'Jan' ] ] ];
+
+console.log(arrays[(1)[(3)[0]]]); //??
