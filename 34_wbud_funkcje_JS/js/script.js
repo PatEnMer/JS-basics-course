712ed262fa9ce738f281509309989b11e80161c5
PatEnMer <patryk.nowak@gmail.com>
2019-10-30 14:48:02 +0100

    built-in functions

---
+++ 34_wbud_funkcje_JS/js/script.js
@@ -0,0 +1,11 @@
+// eval('alert("Witaj");'); //parsuje tekst do kodu
+
+// console.log(isFinite(Infinity)); //sprawdza czy dana liczba jest liczbą skończoną
+
+// console.log(!isNaN(3 * undefined)); //sprawdza czy dana nie jest liczbą - !NaN - zaprzeczenie
+
+// console.log(parseFloat('20.5zł')); //parsuje stringa do number, ustawiając daną jako liczbę, pozwybając się zbędnych znaków
+
+// console.log(parseInt('32.12')); //parsuje przekazany string na number
+
+alert('witaj'); //to funkcja dostępna z poziomu przeglądarki
