fb1aca2338e8af95c1821c3e94ace580e770f76a
PatEnMer <patryk.nowak@gmail.com>
2019-11-04 14:03:25 +0100

    object math

---
+++ 40_obiekt_math/js/script.js
@@ -0,0 +1,10 @@
+//Math nie jest funkcją, nie wywołujemy jej
+// console.log(Math);
+
+console.log(Math.abs(-22)); //zwraca wartość absolutną podanego argumentu
+console.log(Math.round(22.745234));
+console.log(Math.ceil(22.01)); //zaokrągla zawsze w górę
+console.log(Math.floor(24.92)); //zaokrągla zawsze w dół
+console.log(Math.max(3, 4, 8, 1, 0)); //zwracam maksymalną wartość
+console.log(Math.min(231, 234, 23, 234)); // zwraca minimalną warotść
+console.log(Math.round(Math.random() * 10)); //losowanie liczby od 0 do 10
