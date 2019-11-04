9fa99126d035aab277d1538b0025e577ed843e63
PatEnMer <patryk.nowak@gmail.com>
2019-11-04 12:59:49 +0100

    object array

---
+++ 38_obiekt_array/js/script.js
@@ -0,0 +1,7 @@
+let arr = [];
+
+let arr1 = new Array(1, 2); //podanie kilku parametrów dla konstruktora Array oznacza, że chcemy utworzyć tablice z takimi właśnie wartościami
+let arr2 = new Array(10); //korzystanie z konstruktora Array z podaniem jednego parametru, oznacza utworzenie pustej tablicy z ilością indeksów podanych w parametrze
+
+console.log(arr1);
+console.log(arr2);
