bafff69a1534fe1eeb31fc519875c0d21dc43640
PatEnMer <patryk.nowak@gmail.com>
2019-11-08 13:45:05 +0100

    other properties

---
+++ 54_inne_wlasciwosci_elementow/js/script.js
@@ -0,0 +1,14 @@
+let list = document.querySelector('#list');
+
+let first = list.firstChild;
+
+let firstElement = list.firstElementChild;
+
+let last = list.lastChild;
+
+//nodeType:
+// 1 element
+// 3 text
+// 8 comment
+// 9 document
+// 11 fragment
