677d70294eaa32b90f899da515977423e971e281
PatEnMer <patryk.nowak@gmail.com>
2019-11-25 14:11:30 +0100

    conditional function call

--- 73_tips_n_tricks/js/script.js
+++ 73_tips_n_tricks/js/script.js
@@ -1,12 +1,3 @@
-let text = 'Odrwócony tekst do przeczytania całkiem całkiem';
-
-let reverse = '';
-
-for (let i = text.length - 1; i >= 0; i--) {
-	reverse += text[i];
+function sayHello() {
+	alert('Siema');
 }
-
-console.log(reverse);
-
-text1 = 'dowolną wartość, prawdziwą lub fałszywą skonwertuj na fałsz';
-console.log(text1.split('').reverse().join(''));
