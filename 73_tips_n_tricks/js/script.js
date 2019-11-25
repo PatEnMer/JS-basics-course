c8dd020776961241b4f33bd94eb79f56edc0bee6
PatEnMer <patryk.nowak@gmail.com>
2019-11-25 16:03:47 +0100

    add

--- 73_tips_n_tricks/js/script.js
+++ 73_tips_n_tricks/js/script.js
@@ -1,17 +1,63 @@
-function getValues() {
-	let obj = {
-		firstName: 'Pioter',
-		lastName: 'Nosacz'
-	};
+// function getValues() {
+// 	let obj = {
+// 		firstName: 'Pioter',
+// 		lastName: 'Nosacz'
+// 	};
 
-	let id = 2345234;
+// 	let id = 2345234;
 
-	return [ obj, id ];
-}
+// 	return [ obj, id ];
+// }
 
-let values = getValues();
-let data = values[0];
-let id = values[1];
+// let values = getValues();
+// let data = values[0];
+// let id = values[1];
 
-console.log(data);
-console.log(id);
+// console.log(data);
+// console.log(id);
+
+// function sayHello() {
+// 	alert('Siema');
+// }
+
+// let text = 'Odrwócony tekst do przeczytania całkiem całkiem';
+
+// let reverse = '';
+
+// for (let i = text.length - 1; i >= 0; i--) {
+// 	reverse += text[i];
+// }
+
+// console.log(reverse);
+
+// text1 = 'dowolną wartość, prawdziwą lub fałszywą skonwertuj na fałsz';
+// console.log(text1.split('').reverse().join(''));
+
+// const txt = '';
+
+// let b = Boolean(txt);
+
+// // !! - dowolną wartość, prawdziwą lub fałszywą skonwertuj na fałsz - podwójne zaprzeczenie
+// if (!!txt === false) {
+// 	console.log('fałsz');
+// }
+
+// function sayHello(name) {
+// 	name = name || 'Nieznajomy'; //zapis poniższy można zastąpić przez "or"
+// 	// name = name ? name : 'Nieznajomy';
+// 	alert('Witaj, ' + name);
+// }
+
+// sayHello('Pat');
+
+// let text = '';
+
+// for (let i = 0; i < 100; i++) {
+// 	text += '-';
+// }
+
+// console.log(text);
+
+// let text1 = new Array(101).join('x');
+
+// console.log(text1);
