ef58a8161c7a435d0a6246846c007f698d33b2aa
PatEnMer <patryk.nowak@gmail.com>
2019-11-25 13:47:29 +0100

    value assigment

--- 73_tips_n_tricks/js/script.js
+++ 73_tips_n_tricks/js/script.js
@@ -1,8 +1,16 @@
-const txt = '';
+// const txt = '';
 
-let b = Boolean(txt);
+// let b = Boolean(txt);
 
-// !! - dowolną wartość, prawdziwą lub fałszywą skonwertuj na fałsz - podwójne zaprzeczenie
-if (!!txt === false) {
-	console.log('fałsz');
+// // !! - dowolną wartość, prawdziwą lub fałszywą skonwertuj na fałsz - podwójne zaprzeczenie
+// if (!!txt === false) {
+// 	console.log('fałsz');
+// }
+
+function sayHello(name) {
+	name = name || 'Nieznajomy'; //zapis poniższy można zastąpić przez "or"
+	// name = name ? name : 'Nieznajomy';
+	alert('Witaj, ' + name);
 }
+
+sayHello('Pat');
