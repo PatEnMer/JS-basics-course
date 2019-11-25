4400cd7cb16249fc13c0200778bd7d47b2b2094e
PatEnMer <patryk.nowak@gmail.com>
2019-11-25 14:20:36 +0100

    returning many function values

--- 73_tips_n_tricks/js/script.js
+++ 73_tips_n_tricks/js/script.js
@@ -1,3 +1,17 @@
-function sayHello() {
-	alert('Siema');
+function getValues() {
+	let obj = {
+		firstName: 'Pioter',
+		lastName: 'Nosacz'
+	};
+
+	let id = 2345234;
+
+	return [ obj, id ];
 }
+
+let values = getValues();
+let data = values[0];
+let id = values[1];
+
+console.log(data);
+console.log(id);
