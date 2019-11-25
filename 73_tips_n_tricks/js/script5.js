448495f02e4c0940cb4f941ee56bf423f20cb160
PatEnMer <patryk.nowak@gmail.com>
2019-11-25 16:31:28 +0100

    date comparison

---
+++ 73_tips_n_tricks/js/script5.js
@@ -0,0 +1,8 @@
+const date1 = new Date(2019, 2, 12);
+const date2 = new Date(2019, 9, 13);
+
+console.log(date1.getTime() > date2.getTime());
+
+console.log(date1.getTime() < date2.getTime());
+
+console.log(date1.getTime() === date2.getTime());
